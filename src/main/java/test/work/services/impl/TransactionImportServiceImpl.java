c483b86d6bf66d022b06c0c72ee9c05dd4519a27
Jerry Svärdstål <svaerdstaal@gmail.com>
2019-03-30 22:23:48 +0100

    when path is bad, expect fals and error is logged

--- src/main/java/test/work/services/impl/TransactionImportServiceImpl.java
+++ src/main/java/test/work/services/impl/TransactionImportServiceImpl.java
@@ -1,18 +1,32 @@
 package test.work.services.impl;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import test.work.services.TransactionImportService;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.file.Path;
+
+import static java.nio.file.Files.newBufferedReader;
 
 @Getter
 @Service
 public class TransactionImportServiceImpl implements TransactionImportService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TransactionImportServiceImpl.class);
+
 	@Override
 	public boolean process(Path fileToImport) {
-		return true;
+		try(BufferedReader bufferedReader = newBufferedReader(fileToImport)) {
+			return true;
+		}
+		catch(IOException e) {
+			LOGGER.error("Error occurred while loading file: {}", fileToImport.getFileName(), e);
+			return false;
+		}
 	}
 
 }
--- src/test/java/test/work/services/impl/TransactionImportServiceImplTest.java
+++ src/test/java/test/work/services/impl/TransactionImportServiceImplTest.java
@@ -1,7 +1,18 @@
 package test.work.services.impl;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
 import lombok.Getter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -11,8 +22,12 @@ import test.work.services.TransactionImportService;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 
 @Getter
 @SpringBootTest
@@ -29,13 +44,41 @@ class TransactionImportServiceImplTest {
 
 	}
 
+
+	@Mock
+	private Appender<ILoggingEvent> mockAppender;
+	@Captor
+	private ArgumentCaptor<LoggingEvent> captorLoggingEvent;
+	private Logger logger;
 	@Autowired
 	private TransactionImportService transactionImportService;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+		logger.addAppender(mockAppender);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		logger.detachAppender(mockAppender);
+	}
 
 	@Test
 	void whenImportingFile_thenExpectTrue() throws IOException {
 		Path processFile = new ClassPathResource("files/tiny.csv").getFile().toPath();
 		assertTrue(getTransactionImportService().process(processFile));
+	}
+
+	@Test
+	void whenPathIsBad_thenExpectFalse_andErrorIsLogged() throws IOException {
+		Path processFile = Paths.get("files/bad_file.csv");
+		assertFalse(getTransactionImportService().process(processFile));
+
+		verify(mockAppender).doAppend(captorLoggingEvent.capture());
+		final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+		assertEquals(loggingEvent.getLevel(), Level.ERROR);
+		assertEquals("Error occurred while loading file: bad_file.csv", loggingEvent.getFormattedMessage());
 	}
 
 }
